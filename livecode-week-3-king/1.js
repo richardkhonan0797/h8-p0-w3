7f1f797cb017af20f3a2b5cd7f47887d385a2f56
Richard Khonan <richardkhonan0797@gmail.com>
2019-10-04 12:22:22 +0700

    perbaikan 1 dan 2

--- livecode-week-3-king/1.js
+++ livecode-week-3-king/1.js
@@ -11,9 +11,17 @@ bola ping-pong berwarna merah dan putih masing-masing di dalam kotak A.
 
 ===== jawaban =====
 
-set countMerah = 0
-set countPutih = 0
-set warnaBola = [50 value of either merah or putih]
+SET countMerah = 0
+SET countPutih = 0
+SET warnaBola = [50 value of either merah or putih]
+
+FOR bola = 1 to 50
+    IF warnaBola = merah THEN
+        CALCULATE countMerah + 1
+    ELSE IF warnaBola = putih THEN
+        CALCULATE countPutih + 1
+END FOR
+DISPLAY countMerah and countPutih
 
 for(i=0;i<warnaBola.length;i++){
     if(warnaBola[i] == 'merah')
