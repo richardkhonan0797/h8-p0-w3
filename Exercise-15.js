c8536e7581651f8e548cd77bbbf592dd16f62674
Richard Khonan <richardkhonan0797@gmail.com>
2019-10-04 11:58:42 +0700

    perbaikan Exercise 15

--- Exercise-15.js
+++ Exercise-15.js
@@ -1,28 +1,22 @@
 function groupAnimals(animals) {
-    // you can only write your code here!
-    var newArr = [];
-    var temp = [];
-    animals.sort();
-    index = 0;
-    
-    for(i=0;i<animals.length;i++){
-      if(i===0){
-        temp.push(animals[i]);
-      }else if(animals[i][0]===animals[i-1][0]){
-        temp.push(animals[i]);
-      }else{
-        newArr.push(temp);
-        temp = [];
-        temp.push(animals[i]);
+  // you can only write your code here!
+  var res = []
+  var temp = []
+  for(var i=92;i<=122;i++){
+    for(var j=0;j<animals.length;j++){
+      if(animals[j].charCodeAt(0)===i){
+        temp.push(animals[j])
       }
     }
-    newArr.push(temp);
-    return newArr;
+    if(temp.length!==0)
+      res.push(temp)
+    temp = []
   }
-  
-  //['ayam','anoa','cacing','kuda','kancil']
-  // TEST CASES
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ,'babi']));
-  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+  return res
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
